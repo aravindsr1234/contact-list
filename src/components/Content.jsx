c6c113d1406ec8cc583d80350c6f53429bd17060
aravind <aravind.stackup@gmail.com>
2023-10-04 14:09:02 +0530

    search

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -13,15 +13,21 @@ function Content() {
         getData();
     }, []);
 
-    // useEffect(() => {
+    let currentPage = 1;
+    let itemsPerPage = 10;
+
     const getData = () => {
-        axios.get('http://localhost:3001/get')
+        axios.get('http://localhost:3001/get', {
+            params: {
+                page: currentPage,
+                limit: itemsPerPage,
+            }
+        })
             .then((res) => {
                 console.log(res.data);
                 setData(res.data);
             })
     };
-    // }, []);
 
     /**
      * Author: Aravind
@@ -32,50 +38,185 @@ function Content() {
         lastName: "",
         number: ""
     });
-    console.log(postData);
+    const [errors, setErrors] = useState({});
 
     const handleChange = (e) => {
         const value = e.target.value;
+        const inputName = e.target.name;
         console.log(value);
         setPostData({
             ...postData,
-            [e.target.name]: value
+            [inputName]: value
         });
+
+        const updatedErrors = { ...errors };
+        if (inputName === 'firstName') {
+
+            const pattern = /^[a-zA-Z]+$/;
+            updatedErrors.firstName = !pattern.test(value)
+                ? 'First name should only contain letters'
+                : '';
+        } else if (inputName === 'lastName') {
+
+            const pattern = /^[a-zA-Z0-9]+$/;
+            updatedErrors.lastName = !pattern.test(value)
+                ? 'Last name should contain letters and numbers only'
+                : '';
+        } else if (inputName === 'number') {
+
+            const pattern = /^[0-9]+$/;
+            updatedErrors.number = !pattern.test(value)
+                ? 'Number should contain numbers only'
+                : '';
+        }
+        setErrors(updatedErrors);
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const userData = {
-            firstName: postData.firstName,
-            lastName: postData.lastName,
-            number: postData.number
-        }
-        try {
-            axios.post("http://localhost:3001", userData)
-                .then((res) => {
-                    console.log(res);
-                    getData();
-                    setPostData({
-                        firstName: "",
-                        lastName: "",
-                        number: "",
+        if (!errors.firstName && !errors.lastName && !errors.number) {
+
+            const userData = {
+                firstName: postData.firstName,
+                lastName: postData.lastName,
+                number: postData.number
+            }
+            try {
+                axios.post("http://localhost:3001", userData)
+                    .then((res) => {
+                        console.log(res);
+                        getData();
+                        setPostData({
+                            firstName: "",
+                            lastName: "",
+                            number: "",
+                        })
                     })
-                })
-        } catch (error) {
-            console.log(error);
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            console.log('Please correct the form errors.');
         }
     };
 
+    /**
+     * Author: Aravind
+     * desc: edit data in the mongodb
+     */
+    const getEditData = (id) => {
+        axios.get(`http://localhost:3001/get/?id=${id}`)
+            .then(response => {
+                console.log("get data to edit", response);
+                setPostData({ ...postData, firstName: response.data.firstName, lastName: response.data.lastName, number: response.data.number, id: response.data._id });
+            })
+    }
+
+    const editData = (id) => {
+        axios.put(`http://localhost:3001/update/?id=${id}`, postData)
+            .then(response => {
+                console.log("update", response);
+                getData();
+                setPostData({
+                    firstName: "",
+                    lastName: "",
+                    number: "",
+                })
+            })
+    }
+
+    /**
+     * Author: Aravind
+     * desc: delelte data in the mongodb
+     */
+    const deleteData = (id) => {
+        axios.delete(`http://localhost:3001/delete/?id=${id}`)
+            .then(response => {
+                console.log("delete", response);
+                getData();
+            })
+    }
+
+    /**
+     * Author: Aravind
+     * desc: search data in the mongodb
+     */
+    const [searched, setSearched] = useState({});
+    console.log(searched);
+    const search = (e) => {
+        const query = e.target.value;
+        console.log(query);
+        setSearched(query)
+    }
+
+    useEffect(() => {
+        myr();
+    }, [])
+    const myr = () => {
+        axios.get(`http://localhost:3001/search/?term=${searched}`)
+            .then((res) => {
+                console.log(res.data)
+                if (res.data.length !== 0) {
+                    setData(res.data);
+                } else {
+                    getData();
+                }
+            })
+    }
+
     return (
         <div className="container">
-            {/* button for Add contacts
+            <div className="forAdd">
+                {/* button for Add contacts
             <!-- Button trigger modal --> */}
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Add Contacts
-            </button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Add Contacts
+                </button>
 
-            {/* <!-- Modal --> */}
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                {/* <!-- Add Modal --> */}
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Add Contacts</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form action="" onSubmit={handleSubmit}>
+                                    <div className="input_row">
+                                        <label htmlFor="">FirstName:</label>
+                                        <input type="text" name="firstName" value={postData.firstName} onChange={handleChange} placeholder="FirstName" />
+                                        {errors.firstName && <div className="error" style={{ color: 'red' }}>{errors.firstName}</div>}
+                                    </div>
+                                    <div className="input_row">
+                                        <label htmlFor="">LastName:</label>
+                                        <input type="text" name="lastName" value={postData.lastName} onChange={handleChange} placeholder="LastName" />
+                                        {errors.lastName && <div className="error" style={{ color: 'red' }}>{errors.lastName}</div>}
+                                    </div>
+                                    <div className="input_row">
+                                        <label htmlFor="">Number:</label>
+                                        <input type="text" name="number" value={postData.number} onChange={handleChange} placeholder="Phone Number" />
+                                        {errors.number && <div className="error" style={{ color: 'red' }}>{errors.number}</div>}
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" class="btn btn-primary">Save</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Add MODAL END */}
+
+                <div>
+                    <input type="search" placeholder="Search" onChange={search} onKeyDown={myr} />
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+
+            {/* <!-- Edit Modal --> */}
+            <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -83,7 +224,7 @@ function Content() {
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <form action="" onSubmit={handleSubmit}>
+                            <form action="">
                                 <div className="input_row">
                                     <label htmlFor="">FirstName:</label>
                                     <input type="text" name="firstName" value={postData.firstName} onChange={handleChange} placeholder="FirstName" />
@@ -98,14 +239,15 @@ function Content() {
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="submit" class="btn btn-primary">Save</button>
+                                    <button type="button" class="btn btn-primary" onClick={() => { editData(postData.id) }}>Save</button>
                                 </div>
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
-            {/* MODAL END */}
+            {/* Edit MODAL END */}
+
 
             <table class="table">
                 <thead>
@@ -126,8 +268,8 @@ function Content() {
                                 <td>{item.lastName}</td>
                                 <td>{item.number}</td>
                                 <td>
-                                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button>
-                                    <button type="button" className="btn btn-danger">Delete</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#editModal" onClick={() => getEditData(item._id)}>edit</button>
+                                    <button type="button" className="btn btn-danger" onClick={() => { deleteData(item._id) }}>Delete</button>
                                 </td>
                             </tr>
                         ))
@@ -138,6 +280,7 @@ function Content() {
                     )}
                 </tbody>
             </table>
+
         </div>
     )
 }
